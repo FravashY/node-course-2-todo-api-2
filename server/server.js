13e68d8b5ba85bb48ff9da5cb5cda995b355d5e4
FravashY <fravashy@gmail.com>
2018-09-04 23:14:26 +0300

    Add tests for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ app.patch('/todos/:id', (req, res) => {
       return res.status(404).send()
     }
 
-    res.send({todo})
+    res.status(200).send({todo})
   }).catch((e) => {
     res.status(400).send()
   })
